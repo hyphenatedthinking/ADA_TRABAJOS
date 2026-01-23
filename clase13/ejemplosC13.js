826642814083b38ef6f758f5428974e22b8cf8af
hyphenatedthinking <josefinamaro178@gmail.com>
2026-01-23 01:12:00 +0100

    Ultimo update ejercicios

--- clase13/ejemplosC13.js
+++ clase13/ejemplosC13.js
@@ -3,7 +3,17 @@
 // Crea una función declarada llamada calcularAreaTriangulo que reciba dos parámetros: 
 // la base y la altura de un triángulo. La función debe devolver el área del triángulo.
 
-function calcularAreaTriangulo (){
+const prompt = require('prompt-sync')();
 
-    
+let base = parseFloat(prompt("Ingrese la base del triángulo: "));
+let altura = parseFloat(prompt("Ingrese la altura del triángulo: "));
+
+function calcularAreaTriangulo(base, altura) {
+    return (base * altura) / 2;
 }
+
+// llamar a la función
+let areaTriangulo = calcularAreaTriangulo(base, altura);
+
+// mostrar el resultado
+console.log(`El área del triángulo es: ${areaTriangulo}`);
